77d3c96871059ba7c93911b59c5ebd389186421f
M09Ic <m09ician@gmail.com>
2024-03-04 21:05:28 +0800

    add engine DetectResponse

--- engine.go
+++ engine.go
@@ -1,14 +1,20 @@
 package fingers
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/chainreactors/fingers/common"
 	"github.com/chainreactors/fingers/fingerprinthub"
 	"github.com/chainreactors/fingers/fingers"
 	wappalyzer "github.com/chainreactors/fingers/wappalyzer"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 func NewEngine() (*Engine, error) {
 	engine := &Engine{}
-	fingersEngine, err := fingers.NewFingersEngine(fingers.FingerData)
+	fingersEngine, err := fingers.NewFingersEngine(fingers.HTTPFingerData, fingers.SocketFingerData)
 	if err != nil {
 		return nil, err
 	}
@@ -30,7 +36,46 @@ func NewEngine() (*Engine, error) {
 }
 
 type Engine struct {
-	FingersEngine     *fingers.FingersRules
-	FingerPrintEngine fingerprinthub.FingerPrintHubs
-	WappalyzerEngine  *wappalyzer.Wappalyze
+	FingersEngine     *fingers.FingersRules          // support http/socket
+	FingerPrintEngine fingerprinthub.FingerPrintHubs // support http
+	WappalyzerEngine  *wappalyzer.Wappalyze          // support http
+}
+
+func (engine *Engine) DetectResponse(resp *http.Response) (common.Frameworks, error) {
+	var raw bytes.Buffer
+
+	raw.WriteString(fmt.Sprintf("%s %s\r\n", resp.Proto, resp.Status))
+	for k, v := range resp.Header {
+		for _, i := range v {
+			raw.WriteString(fmt.Sprintf("%s: %s\r\n", k, i))
+		}
+	}
+	raw.WriteString("\r\n")
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	raw.Write(body)
+	_ = resp.Body.Close()
+	frames := make(common.Frameworks)
+	if engine.FingersEngine != nil {
+		var cert string
+		if resp.TLS != nil && len(resp.TLS.PeerCertificates) > 0 {
+			cert = strings.Join(resp.TLS.PeerCertificates[0].DNSNames, ",")
+		}
+		fs, _ := engine.FingersEngine.HTTPMatch(raw.Bytes(), cert)
+		frames.Merge(fs)
+	}
+
+	if engine.FingerPrintEngine != nil {
+		fs := engine.FingerPrintEngine.Match(resp.Header, string(body))
+		frames.Merge(fs)
+	}
+
+	if engine.WappalyzerEngine != nil {
+		fs := engine.WappalyzerEngine.Fingerprint(resp.Header, body)
+		frames.Merge(fs)
+	}
+
+	return frames, err
 }
