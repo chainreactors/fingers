5de9b96078618c5753ea0b6269943b26ec21d4f2
HuYlllc <632781087@qq.com>
2025-11-05 17:48:12 +0800

    fix: json schema error

--- common/attributes.go
+++ common/attributes.go
@@ -27,17 +27,17 @@ func NewAttributesWithCPE(s string) *Attributes {
 }
 
 type Attributes struct {
-	Part      string `json:"part" yaml:"part"`
-	Vendor    string `json:"vendor" yaml:"vendor"`
-	Product   string `json:"product" yaml:"product"`
-	Version   string `json:"version,omitempty" yaml:"version,omitempty"`
-	Update    string `json:"update,omitempty" yaml:"update,omitempty"`
-	Edition   string `json:"edition,omitempty" yaml:"edition,omitempty"`
-	SWEdition string `json:"sw_edition,omitempty" yaml:"sw_edition,omitempty"`
-	TargetSW  string `json:"target_sw,omitempty" yaml:"target_sw,omitempty"`
-	TargetHW  string `json:"target_hw,omitempty" yaml:"target_hw,omitempty"`
-	Other     string `json:"other,omitempty" yaml:"other,omitempty"`
-	Language  string `json:"language,omitempty" yaml:"language,omitempty"`
+	Part      string `json:"part,omitempty" yaml:"part" jsonschema:"title=Part,description=Component part identifier"`
+	Vendor    string `json:"vendor,omitempty" yaml:"vendor" jsonschema:"title=Vendor,description=Vendor or manufacturer name"`
+	Product   string `json:"product,omitempty" yaml:"product" jsonschema:"title=Product,description=Product name"`
+	Version   string `json:"version,omitempty" yaml:"version,omitempty" jsonschema:"title=Version,description=Product version"`
+	Update    string `json:"update,omitempty" yaml:"update,omitempty" jsonschema:"title=Update,description=Product update version"`
+	Edition   string `json:"edition,omitempty" yaml:"edition,omitempty" jsonschema:"title=Edition,description=Product edition"`
+	SWEdition string `json:"sw_edition,omitempty" yaml:"sw_edition,omitempty" jsonschema:"title=Software Edition,description=Software edition"`
+	TargetSW  string `json:"target_sw,omitempty" yaml:"target_sw,omitempty" jsonschema:"title=Target Software,description=Target software"`
+	TargetHW  string `json:"target_hw,omitempty" yaml:"target_hw,omitempty" jsonschema:"title=Target Hardware,description=Target hardware"`
+	Other     string `json:"other,omitempty" yaml:"other,omitempty" jsonschema:"title=Other,description=Other attributes"`
+	Language  string `json:"language,omitempty" yaml:"language,omitempty" jsonschema:"title=Language,description=Language identifier"`
 }
 
 func (a *Attributes) WFN() *wfn.Attributes {
